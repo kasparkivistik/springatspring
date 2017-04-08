64667c47e238f4c433c603229573ebab6163bbdb
Lauri Kukk <lakukk@ttu.ee>
2017-04-08 15:57:02 +0300

    Game re

--- src/main/java/eu/leisurify/website/game/GameController.java
+++ src/main/java/eu/leisurify/website/game/GameController.java
@@ -2,6 +2,12 @@ package eu.leisurify.website.game;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class GameController {
@@ -9,5 +15,15 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @RequestMapping(value="game/register/", method=RequestMethod.GET)
+    public ModelAndView register(Map<String, Object> model) {
+        return new ModelAndView("gameregistration/index", "command", new Game());
+    }
+
+    @RequestMapping(value = "/gameregistration/add", method = RequestMethod.POST)
+    public String add(Map<String, Object> model, @ModelAttribute("SpringWeb") Game game) {
+        gameService.saveReagent(game);
+        return "redirect:/";
+    }
 
 }
