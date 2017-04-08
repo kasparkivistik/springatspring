fb5746a7bab77c1c2b37777afa05fdc1f62b27be
Svjatoslav Agejenko <svjatoslav@svjatoslav.eu>
2017-04-08 03:33:36 +0300

    Passibility to register player in the database.

--- src/main/java/eu/leisurify/website/registration/RegistrationController.java
+++ src/main/java/eu/leisurify/website/registration/RegistrationController.java
@@ -1,7 +1,12 @@
 package eu.leisurify.website.registration;
 
 import eu.leisurify.website.player.Player;
+import eu.leisurify.website.player.PlayerService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,14 +16,21 @@ import java.util.Map;
 @Controller
 public class RegistrationController {
 
+    private static final Log LOG = LogFactory.getLog(RegistrationController.class);
+
+    @Autowired
+    PlayerService playerService;
+
     @RequestMapping(value="/registration/", method=RequestMethod.GET)
     public ModelAndView register(Map<String, Object> model) {
         return new ModelAndView("registration/index", "command", new Player());
     }
 
-    @RequestMapping("/registration/add")
-    public String add(Map<String, Object> model) {
-        return "welcome/index";
+    @RequestMapping(value = "/registration/add", method = RequestMethod.POST)
+    public String add(Map<String, Object> model, @ModelAttribute("SpringWeb") Player player) {
+        LOG.debug("Adding new player: " + player);
+        playerService.savePlayer(player);
+        return "redirect:/";
     }
 
 }
--- src/main/webapp/registration/index.jsp
+++ src/main/webapp/registration/index.jsp
@@ -20,15 +20,16 @@
             </div>
         </div>
         <div class="form-group">
-            <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+            <form:label path="email" for="inputEmail" class="col-sm-2 control-label">Email</form:label>
             <div class="col-sm-10">
-                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                <form:input path="email" type="email" class="form-control" id="inputEmail" placeholder="Email"/>
             </div>
         </div>
         <div class="form-group">
-            <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+            <form:label path="password" for="inputPassword" class="col-sm-2 control-label">Password</form:label>
             <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                <form:input path="password" type="password"
+                            class="form-control" id="inputPassword" placeholder="Password"/>
             </div>
         </div>
         <div class="form-group">
